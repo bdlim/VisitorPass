3c77c3d3ff364a6b8cc63766bcfccc8d6fc38b6a
Brandon Lim <brandondlim@gmail.com>
2015-12-13 22:04:03 -0800

    Added Form and Auto Layout. Implemented Keyboard hiding on off-click

--- VisitorPass/ViewController.m
+++ VisitorPass/ViewController.m
@@ -10,18 +10,75 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *firstName;
+@property (weak, nonatomic) IBOutlet UITextField *lastName;
+@property (weak, nonatomic) IBOutlet UITextField *company;
+@property (weak, nonatomic) IBOutlet UITextField *companyAddress;
+@property (weak, nonatomic) IBOutlet UITextField *zipCode;
+@property (weak, nonatomic) IBOutlet UITextField *employeeSponsor;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.firstName.delegate = self;
+    self.lastName.delegate = self;
+    self.company.delegate = self;
+    self.companyAddress.delegate = self;
+    self.zipCode.delegate = self;
+    self.zipCode.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
+    self.employeeSponsor.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    if (textField == self.employeeSponsor) {
+        textField.returnKeyType = UIReturnKeyDone;
+    } else {
+        textField.returnKeyType = UIReturnKeyNext;
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.firstName) {
+        [textField resignFirstResponder];
+        [self.lastName becomeFirstResponder];
+    } else if (textField == self.lastName) {
+        [textField resignFirstResponder];
+        [self.company becomeFirstResponder];
+    } else if (textField == self.company) {
+        [textField resignFirstResponder];
+        [self.companyAddress becomeFirstResponder];
+    } else if (textField == self.companyAddress) {
+        [textField resignFirstResponder];
+        [self.zipCode becomeFirstResponder];
+    } else if (textField == self.zipCode) {
+        [textField resignFirstResponder];
+        [self.employeeSponsor becomeFirstResponder];
+    } else {
+        // Submit
+    }
+    return YES;
+}
+
+- (IBAction)submit:(id)sender {
+    // Submit
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.firstName resignFirstResponder];
+    [self.lastName resignFirstResponder];
+    [self.company resignFirstResponder];
+    [self.companyAddress resignFirstResponder];
+    [self.zipCode resignFirstResponder];
+    [self.employeeSponsor resignFirstResponder];
 }
 
 @end
